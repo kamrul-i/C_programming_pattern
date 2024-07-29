3294659e5edc5cbac0465b5bbd5b5f9c746bd864
offic <offic@DESKTOP-GLF4UNU>
2024-07-29 19:26:16 +0600

    Welcome! Introduction to "C-programming pattern".

---
+++ src/main/java/com/kamrul/c_programming_pattern/C_programming_pattern.java
@@ -0,0 +1,8 @@
+package com.kamrul.c_programming_pattern;
+public class C_programming_pattern {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Welcome! Introduction to \"C-programming pattern\"");
+    }
+}

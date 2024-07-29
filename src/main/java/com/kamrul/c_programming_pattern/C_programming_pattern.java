b5b34899b7c103dbbc9a52009e214e1a2f78ce9a
offic <offic@DESKTOP-GLF4UNU>
2024-07-29 20:43:35 +0600

    Left-side, Triangle pattern design.

--- src/main/java/com/kamrul/c_programming_pattern/C_programming_pattern.java
+++ src/main/java/com/kamrul/c_programming_pattern/C_programming_pattern.java
@@ -10,9 +10,14 @@ public class C_programming_pattern {
         System.out.print("Enter any number : ");
         num1 = input.nextInt();
 
-        for (row = num1; row >= 1; row--) {
+        for (row = 1; row <= num1; row++) {
+            
+            for ( col = 1; col <=num1-row; col++) {
+                System.out.print("  ");  //----------(2)
+            }
+            
             for (col = 1; col <= row; col++) {
-                System.out.printf("%c ", col + 64);  //----------(1)
+                System.out.printf("%d ", col); //-----(1)
             }
             System.out.printf("\n");
         }
@@ -24,9 +29,14 @@ public class C_programming_pattern {
         System.out.print("Enter any number : ");
         num2 = input.nextInt();
 
-        for (row = num2; row >= 1; row--) {
+        for (row = 1; row <= num2; row++) {
+            
+            for ( col=1; col <=num2-row; col++) {
+                System.out.print("  "); //----------(2)
+            }
+            
             for (col = 1; col <= row; col++) {
-                System.out.printf("%c ", row + 96);  //----------(1)
+                System.out.printf("%c ", row + 64); //----(1)
             }
             System.out.printf("\n");
         }
@@ -34,7 +44,7 @@ public class C_programming_pattern {
     }
 
     /*
-    উপরোক্ত code দ্বারা Right-side down, Triangle pattern design করা হয়েছে।।
+    উপরোক্ত code দ্বারা Left-side, Triangle pattern design করা হয়েছে।।
     
     Pattern মুলত C_Programming এর অংশ, যদিও এখানে java-তে code করা হয়েছে।।
     এখানে column print করা হয়েছে, row print করতে চাইলে ১নং সমীকরণে col এর পরিবর্তে row লিখুন।।
@@ -45,8 +55,9 @@ public class C_programming_pattern {
         System.out.printf("%c ", col+64); এ line use করে ফলাফলে capital character দেখুন।।
         System.out.printf("%c ", col+96); এ line use করে ফলাফলে small character দেখুন।।
         System.out.print("* "); এ line use করে ফলাফলে spacial any character দেখুন।। *(স্টার) এর যায়গায় যা দিবেন, তাই দেখতে পাবেন।।
-
         যেখানে printf কাজ করবে না, সেখানে print ব্যবহার করুন।।
     
+    2নং সমীকরণে space print করা হয়েছে,
+        Left-side যেকোন কিছু design করতে হলে, প্রথমে space print করতে হবে।।
      */
 }
